6e1b6e77b63349821d368cce2ef942783e4aedab
Yousef <yousefborhanii@gmail.com>
2023-07-03 19:28:23 +0330

    Implement server connecting

---
+++ Assets/Scripts/PUN/ConnectToServer.cs
@@ -0,0 +1,25 @@
+using System;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PUN
+{
+    public class ConnectToServer : MonoBehaviourPunCallbacks
+    {
+        private void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            PhotonNetwork.JoinLobby();
+        }
+
+        public override void OnJoinedLobby()
+        {
+            SceneManager.LoadScene("Lobby");
+        }
+    }
+}
